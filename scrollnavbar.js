bfa4f94e99b435857e4ce2ea0b6dccb9b74e9420
ImEvoke <58148396+ImEvoke@users.noreply.github.com>
2023-04-30 18:54:25 -0500

    Drop down nav bar

--- scrollnavbar.js
+++ scrollnavbar.js
@@ -1,4 +1,8 @@
 window.addEventListener("scroll", function(){
   var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 100)
+  header.classList.toggle("sticky", window.scrollY > 150)
+})
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("stickydown", window.scrollY > 150)
 })
