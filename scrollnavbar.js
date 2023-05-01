c4a1c6e1a9948954a6a4099a4a0f924790df4f87
ImEvoke <58148396+ImEvoke@users.noreply.github.com>
2023-04-30 22:46:16 -0500

    Updated nav bar

--- scrollnavbar.js
+++ scrollnavbar.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", function(){
   var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 150)
+  header.classList.toggle("sticky", window.scrollY > 20)
 })
 window.addEventListener("scroll", function(){
   var header = document.querySelector("header");
-  header.classList.toggle("stickydown", window.scrollY > 150)
+  header.classList.toggle("stickydown", window.scrollY > 20)
 })
